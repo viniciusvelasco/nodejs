b800556003a42d89b43164b0b53ae15570f85d7e
Vinicius Alves Velasco <vinicius.velasco@amaggi.com.br>
2019-02-06 13:31:47 -0300

    Estrutura do server

--- src/app.js
+++ src/app.js
@@ -1,16 +1,7 @@
-'use strict'
-
-const http = require('http');
-const debug = require('debug')('nodestr:server');
 const express = require('express');
 
 const app = express();
-const port = 3000;
-app.set('port', port);
-
-const server = http.createServer(app);
 const router = express.Router();
-
 const route = router.get('/', (req, res, next) => {
     res.status(200).send({
         title: 'Node Store API',
@@ -19,5 +10,4 @@ const route = router.get('/', (req, res, next) => {
 });
 app.use('/', route);
 
-server.listen(port);
-console.log('API rodando na porta ' + port);
+module.exports = app;
