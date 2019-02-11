ce07004e6f09c9ff610b9e77c70fdb9f46591cf6
Vinicius Alves Velasco <vinicius.velasco@amaggi.com.br>
2019-02-11 12:19:39 -0300

    Configuração do mongoose

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
+mongoose.connect('mongodb://vinicius:vinicius@node-store-shard-00-00-nrnkp.azure.mongodb.net:27017,node-store-shard-00-01-nrnkp.azure.mongodb.net:27017,node-store-shard-00-02-nrnkp.azure.mongodb.net:27017/test?ssl=true&replicaSet=node-store-shard-0&authSource=admin&retryWrites=true');
 
 const index = require('./routes/index');
 const product = require('./routes/product');
