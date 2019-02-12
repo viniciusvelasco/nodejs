232f4e37aaeed95f5a69c3c047c8095be5aeec9a
Vinicius Alves Velasco <vinicius.velasco@amaggi.com.br>
2019-02-12 12:20:36 -0300

    Exluindo produto

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -44,7 +44,13 @@ exports.put = ('/:id', (req, res, next) => {
 });
 
 exports.delete = ('/', (req, res, next) => {
-    res.status(200).send(req.body);
+    Product
+        .findByIdAndRemove(req.params.id)
+        .then(data => {
+            res.status(200).send({ message: 'Produto removido com sucesso' });
+        }).catch(e => {
+            res.status(400).send(e);
+        })
 });
 
 exports.getBySlug = (req, res, next) => {
