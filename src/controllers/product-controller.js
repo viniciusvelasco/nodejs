0d8df9482790cba415066e75a6d14d76833b748e
Vinicius Alves Velasco <vinicius.velasco@amaggi.com.br>
2019-02-12 12:10:06 -0300

    Listando produtos

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -3,6 +3,18 @@
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
+exports.get = (req, res, next) => {
+    Product
+        .find({
+            active: true
+        }, 'title price slug')
+        .then(data => {
+        res.status(200).send(data);
+    }).catch(e => {
+        res.status(400).send({ message: 'Falha ao cadastrar o produto', data: e });
+    });
+}
+
 exports.post = (req, res, next) => {
     var product = new Product(req.body);
     product.save().then(x => {
@@ -23,3 +35,39 @@ exports.put = ('/:id', (req, res, next) => {
 exports.delete = ('/', (req, res, next) => {
     res.status(200).send(req.body);
 });
+
+exports.getBySlug = (req, res, next) => {
+    Product
+        .findOne({
+            slug : req.params.slug,
+            active: true
+        }, 'title description price slug tags')
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e);
+        })
+}
+
+exports.getByTag = (req, res, next) => {
+    Product
+        .find({
+            tags : req.params.tag,
+            active: true
+        }, 'title description price slug tags')
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e);
+        })
+}
+
+exports.getById = (req, res, next) => {
+    Product
+        .findById(req.params.id)
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e);
+        })
+}
