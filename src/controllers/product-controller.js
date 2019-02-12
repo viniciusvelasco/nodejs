2d4b7022976daf0dea193a2b722f382a650a6fdc
Vinicius Alves Velasco <vinicius.velasco@amaggi.com.br>
2019-02-12 12:18:02 -0300

    Alterar produto

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -25,11 +25,22 @@ exports.post = (req, res, next) => {
 }
 
 exports.put = ('/:id', (req, res, next) => {
-    const id = req.params.id;
+    Product
+    .findByIdAndUpdate(req.params.id, {
+        $set: {
+            title: req.body.title,
+            description: req.body.description,
+            price: req.body.price,
+            slug: req.body.slug
+        }
+    })
+    .then(x => {
     res.status(200).send({
-        id: id,
-        item: req.body
+        message: 'Produto atualizado com sucesso!'
     });
+}).catch(e => {
+    res.status(400).send({ message: 'Falha ao atualizar o produto', data: e });
+});
 });
 
 exports.delete = ('/', (req, res, next) => {
